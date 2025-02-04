79e1c6e99b1c39c8d018119ca25c7b850a193173
zhaokaifan02 <zhaokaifan042@gmail.com>
2025-02-04 14:58:30 +0800

    atomic  op

---
+++ lesson8/reduce.cu
@@ -0,0 +1,104 @@
+#include "error.cuh"
+#include <stdio.h>
+
+#ifdef USE_DP
+    typedef double real;
+#else
+    typedef float real;
+#endif
+
+const int NUM_REPEATS = 100;
+const int N = 100000000;
+const int M = sizeof(real) * N;
+const int BLOCK_SIZE = 128;
+
+void timing(const real *d_x);
+
+int main(void)
+{
+    real *h_x = (real *) malloc(M);
+    for (int n = 0; n < N; ++n)
+    {
+        h_x[n] = 1.23;
+    }
+    real *d_x;
+    CHECK(cudaMalloc(&d_x, M));
+    CHECK(cudaMemcpy(d_x, h_x, M, cudaMemcpyHostToDevice));
+
+    printf("\nusing atomicAdd:\n");
+    timing(d_x);
+
+    free(h_x);
+    CHECK(cudaFree(d_x));
+    return 0;
+}
+
+void __global__ reduce(const real *d_x, real *d_y, const int N)
+{
+    const int tid = threadIdx.x;
+    const int bid = blockIdx.x;
+    const int n = bid * blockDim.x + tid; //实际的位置
+    extern __shared__ real s_y[];
+    s_y[tid] = (n < N) ? d_x[n] : 0.0; //单个线程块个位置初始化
+    __syncthreads(); //确保所有线程都完成了这一操偶做
+
+    for (int offset = blockDim.x >> 1; offset > 0; offset >>= 1)
+    {
+        if (tid < offset)
+        {
+            s_y[tid] += s_y[tid + offset];
+        }
+        __syncthreads();
+    }
+
+    if (tid == 0)
+    {
+        atomicAdd(d_y, s_y[0]); //这就是原子操作，原子操作被定义为拿到两个内存，一气呵成的完成操作。
+    }
+}
+
+real reduce(const real *d_x)
+{
+    const int grid_size = (N + BLOCK_SIZE - 1) / BLOCK_SIZE; //避免除不尽bug
+    const int smem = sizeof(real) * BLOCK_SIZE; //指定共享内存大小
+
+    real h_y[1] = {0}; //
+    real *d_y;
+    CHECK(cudaMalloc(&d_y, sizeof(real)));
+    CHECK(cudaMemcpy(d_y, h_y, sizeof(real), cudaMemcpyHostToDevice));
+
+    reduce<<<grid_size, BLOCK_SIZE, smem>>>(d_x, d_y, N);
+
+    CHECK(cudaMemcpy(h_y, d_y, sizeof(real), cudaMemcpyDeviceToHost));
+    CHECK(cudaFree(d_y));
+
+    return h_y[0];
+}
+
+void timing(const real *d_x)
+{
+    real sum = 0;
+
+    for (int repeat = 0; repeat < NUM_REPEATS; ++repeat)
+    {
+        cudaEvent_t start, stop;
+        CHECK(cudaEventCreate(&start));
+        CHECK(cudaEventCreate(&stop));
+        CHECK(cudaEventRecord(start));
+        cudaEventQuery(start);
+
+        sum = reduce(d_x); 
+
+        CHECK(cudaEventRecord(stop));
+        CHECK(cudaEventSynchronize(stop));
+        float elapsed_time;
+        CHECK(cudaEventElapsedTime(&elapsed_time, start, stop));
+        printf("Time = %g ms.\n", elapsed_time);
+
+        CHECK(cudaEventDestroy(start));
+        CHECK(cudaEventDestroy(stop));
+    }
+
+    printf("sum = %f.\n", sum);
+}
+
